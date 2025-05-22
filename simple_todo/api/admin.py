a00c702f2c1c0c2ef41cf9a85a0ce1195042e959
DylanCheetah <dylan_the_cheetah@outlook.com>
2025-05-22 00:30:48 -0400

    Register todo list and task data models with admin site.

--- simple_todo/api/admin.py
+++ simple_todo/api/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Task, TodoList
+
+
+# Model Admin Classes
+# ===================
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ["name", "todo_list", "due_date"]
+    search_fields = ["name"]
+
+
+class TodoListAdmin(admin.ModelAdmin):
+    list_display = ["name", "owner__username"]
+    search_fields = ["name", "owner__username"]
+
+
+# Register data models
+admin.site.register(Task, TaskAdmin)
+admin.site.register(TodoList, TodoListAdmin)
