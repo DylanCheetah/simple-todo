88edc12f3d06b6c18b54bf3ea78afe0a5d07780a
DylanCheetah <dylan_the_cheetah@outlook.com>
2025-05-22 00:11:18 -0400

    Create todo list and task data models.

--- simple_todo/api/models.py
+++ simple_todo/api/models.py
@@ -1,3 +1,15 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+# Data Models
+# ===========
+class TodoList(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=32)
+
+
+class Task(models.Model):
+    todo_list = models.ForeignKey(TodoList, on_delete=models.CASCADE)
+    name = models.CharField(max_length=64)
+    due_date = models.DateField(auto_now=True)
