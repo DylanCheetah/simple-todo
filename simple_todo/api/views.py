95158e5d75fa899a1052a31aab47e53cf02ac0e8
DylanCheetah <dylan_the_cheetah@outlook.com>
2025-05-22 13:42:53 -0400

    Create todo list and task viewsets.

--- simple_todo/api/views.py
+++ simple_todo/api/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from .models import Task, TodoList
+from .serializers import TaskSerializer, TodoListSerializer
+
+
+# Viewset Classes
+# ===============
+class TodoListViewSet(ModelViewSet):
+    serializer_class = TodoListSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return TodoList.objects.filter(owner=self.request.user)
+    
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class TaskViewSet(ModelViewSet):
+    serializer_class = TaskSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        todo_list = self.request.query_params.get("todo_list")
+
+        if todo_list is not None:
+            return Task.objects.filter(owner=self.request.user, todo_list=todo_list)
+        
+        return Task.objects.filter(owner=self.request.user)
